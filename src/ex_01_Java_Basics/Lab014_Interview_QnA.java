156293840907bc1404e196d225fcec139830a34c
saurabhATB14x <saurabhbhattacharjee07@gmail.com>
2025-10-28 23:11:04 +0530

    Java Basis Update

---
+++ src/ex_01_Java_Basics/Lab014_Interview_QnA.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab014_Interview_QnA {
+    public static void main(String[] args) {
+//        int enum = 10;
+        int Enum = 10;
+//        System.out.println(enum);
+        //        int pramod dutta  = 89;
+        int pramod$dutta = 90;
+        int Lab020_Interview_QnA2 = 123;
+
+    }
+}
---
+++ src/ex_05_TypeCasting/Lab057_TypeCasting_Widening.java
@@ -0,0 +1,8 @@
+package ex_05_TypeCasting;
+
+public class Lab057_TypeCasting_Widening {
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = b;  //Valid syntex widening -> Implicit Casting
+    }
+}
