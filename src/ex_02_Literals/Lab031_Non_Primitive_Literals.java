2307731f22745db0b614cce9f9a6f6476956180e
saurabhATB14x <saurabhbhattacharjee07@gmail.com>
2025-10-28 23:42:55 +0530

    Java Literals - 14th Sep

---
+++ src/ex_02_Literals/Lab031_Non_Primitive_Literals.java
@@ -0,0 +1,9 @@
+package ex_02_Literals;
+
+public class Lab031_Non_Primitive_Literals {
+    public static void main(String[] args) {
+        String name = "Pramod"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+
+    }
+}
